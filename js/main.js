311e8f3fa1326303912ad9474bc044522b57d640
enzo <enzovcnt@gmail.com>
2024-12-04 16:54:11 +0100

    add invader, shooter, laser and boom in the style sheet

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ for (let i = 0; i < width * width; i++) {
 
 const squares = Array.from(document.querySelectorAll('.grid div')) //mettre tout les éléments div dans un array
 console.log(squares)
+
+const alienInvaders = [  //séléctionne les index pour leur donner une classe > utilise la boucle pour cela
+    0,1,2,3,4,5,6,7,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+
+function draw(){
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.add('invader');
+    }
+}
+draw()
