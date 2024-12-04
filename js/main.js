494fb2925a0271ddca2db3dc4c572269e636805e
enzo <enzovcnt@gmail.com>
2024-12-04 15:41:16 +0100

    put all te div in an array

--- js/main.js
+++ js/main.js
@@ -7,5 +7,9 @@ const width = 15
 //loop > create 225 square in the div grid
 for (let i = 0; i < width * width; i++) {
     const square = document.createElement('div')
-    grid.appendChild(square)
+    grid.appendChild(square) //elements dans la balise div .grid
 }
+
+
+const squares = Array.from(document.querySelectorAll('.grid div')) //mettre tout les éléments div dans un array
+console.log(squares)
